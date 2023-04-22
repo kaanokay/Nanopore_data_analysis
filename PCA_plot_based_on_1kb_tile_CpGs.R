19b7842cc6ee6cd27f0c4db28f05f317c0d232cc
Kaan Okay <42235353+kaanokay@users.noreply.github.com>
2023-04-22 14:19:44 +0000

    Update PCA_plot_based_on_1kb_tile_CpGs.R

--- PCA_plot_based_on_1kb_tile_CpGs.R
+++ PCA_plot_based_on_1kb_tile_CpGs.R
@@ -53,8 +53,8 @@ Ctr3 <- fread("/media/ko/New Volume/Documents/Nanopore_data/modbam2bed_outputs/U
 colnames(Ctr3) <- c("chrom",	"start", "end", "name", "score", "strand", "tstart", "tend","color", "coverage",
                       "freq", "canon", "mod", "filt")
 
-Dnmt1 <- fread("/media/ko/New Volume/Documents/Nanopore_data/modbam2bed_outputs/UCSC_mouse_genome_based_analysis/Dnmt1_CpGs.cpg.acc.bed")
-colnames(Dnmt1) <- c("chrom",	"start", "end", "name", "score", "strand", "tstart", "tend","color", "coverage",
+# Dnmt1 <- fread("/media/ko/New Volume/Documents/Nanopore_data/modbam2bed_outputs/UCSC_mouse_genome_based_analysis/Dnmt1_CpGs.cpg.acc.bed")
+# colnames(Dnmt1) <- c("chrom",	"start", "end", "name", "score", "strand", "tstart", "tend","color", "coverage",
                     "freq", "canon", "mod", "filt")
 
 Ezh2 <- fread("/media/ko/New Volume/Documents/Nanopore_data/modbam2bed_outputs/UCSC_mouse_genome_based_analysis/Ezh2_CpGs.cpg.acc.bed")
@@ -147,10 +147,10 @@ Ctr3_BSseq <- BSseq(chr = as.character(Ctr3$chrom), pos = Ctr3$start +1,
 
 #
 
-Dnmt1$mod_and_canon <- Dnmt1$mod + Dnmt1$canon
+# Dnmt1$mod_and_canon <- Dnmt1$mod + Dnmt1$canon
 
-Dnmt1_BSseq <- BSseq(chr = as.character(Dnmt1$chrom), pos = Dnmt1$start +1, 
-                    M = matrix(Dnmt1$mod), Cov = matrix(Dnmt1$mod_and_canon), sampleNames = c("Dnmt1"))
+# Dnmt1_BSseq <- BSseq(chr = as.character(Dnmt1$chrom), pos = Dnmt1$start +1, 
+                    # M = matrix(Dnmt1$mod), Cov = matrix(Dnmt1$mod_and_canon), sampleNames = c("Dnmt1"))
 
 #
 
@@ -175,240 +175,69 @@ Kdm6a_BSseq <- BSseq(chr = as.character(Kdm6a$chrom), pos = Kdm6a$start +1,
 
 # --- Generate BSseq objects end ---
 
-# --- tilling each BSseq object to 1kb length size, so each tile has certain number of CpGs start ---
-
-# tilling Chd1
-
-Chd1_1kb_tiled <- tile_by_windows(bs = Chd1_BSseq, win_size = 1000)
-nrow(Chd1_1kb_tiled) # 2,722,502 1kb-length tiles!
-
-# Count overlaps between tiled genome and CpG level genome
-
-table(countOverlaps(Chd1_1kb_tiled, Chd1_BSseq)) # hic CpG icermeyen 197,317 1kb tiled bölge var ama en fazla CpG iceren tiled ise
-# 178 tane CpG iceriyor! bir tane CpG iceren ise 102,876 adet iken 3 tane CpG iceren tile sayısı ise 229,812' dir.
-# Kasper' e not olsun diye start + 1 yapınca pozisyona gercekten de tile basına düsen CpG sayısı degisti; hic CpG icermeyen
-# tile sayısı 197,360 oldu ama önceden 197,317 idi. En fazla CpG iceren tile ise 178 tane CpG iceriyordu simdi bu sayısı 177' ye
-# düstü. 3 tane CpG iceren tile sayısı 229,812' den 229,911 tane oldu!
-
-sum(table(countOverlaps(Chd1_1kb_tiled, Chd1_BSseq))) # this gives total number of 1kb tiles!
-
-number_of_CpGs_per_tile <- as.data.frame(table(countOverlaps(Chd1_1kb_tiled, Chd1_BSseq)))
-colnames(number_of_CpGs_per_tile) <- c("Number_of_CpGs", "Number_of_tiles_having_corresponding_number_of_CpGs")
-
-ggplot(number_of_CpGs_per_tile, aes(x = Number_of_CpGs, y = Number_of_tiles_having_corresponding_number_of_CpGs)) +
-  geom_bar(stat = "identity") +
-  labs(x = "Number_of_CpGs", y = "Number_of_tiles_having_corresponding_number_of_CpGs", title = "1kb tile and CpG overlap") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-number_of_CpGs_per_tile$number_of_total_CpGs <- (as.integer(number_of_CpGs_per_tile$Number_of_CpGs) - 1) * (as.integer(number_of_CpGs_per_tile$Number_of_tiles_having_corresponding_number_of_CpGs))
-sum(number_of_CpGs_per_tile$number_of_total_CpGs) # 21,344,840 CpG var ama Chd1' de normalde 21,344,868 CpG var yani 28 CpG
-# bir sekilde eksilmis oldu bunun sebebi bazen tile tam 1kb olmuyor 999bp de olabiliyor bu yüzden olabilir mi? Bazı CpG' leri
-# bu yüzden kaybetmis olabiliriz! Burada bir sekilde tile sayısı ile tile' ların icerdigi CpG sayısını carparak toplam CpG sayısını
-# yani original olarak BSseq olusturdugumda elde ettigim CpG sayısını elde etmeye calıstım fakat sanıyorum tile' ın mükemmel
-# olarak 1kb yapılamamasından dolayı 28 tane CpG kaybettim!
-
-# Keeping rows with at least 3 CpGs!, that is, keeping 1kb tiles with at least 3 CpGs
-
-Chd1_1kb_tiled_filtered <- Chd1_1kb_tiled[rowSums(as.matrix(countOverlaps(Chd1_1kb_tiled, Chd1_BSseq))) > 2,] # 479,488 tane tile elenmesi
-# lazım ve geriye 2,243,014 adet tile kalması gerekiyor..
-nrow(Chd1_1kb_tiled_filtered) # 2,243,014 adet tile kaldı beklenildigi gibi..
-
-table(countOverlaps(Chd1_1kb_tiled_filtered, Chd1_BSseq)) # Burada hic CpG iceremeyen tile olmaması gerekiyor!
-# Gercekten de yok! Sanırım yaptıgım sey dogru!
-
-# Visualization of number of CpGs and tiles after tile filtering having 2 CpGs and less!
-
-number_of_CpGs_per_tile_2 <- as.data.frame(table(countOverlaps(Chd1_1kb_tiled_filtered, Chd1_BSseq)))
-colnames(number_of_CpGs_per_tile_2) <- c("Number_of_CpGs", "Number_of_tiles_having_corresponding_number_of_CpGs")
-
-ggplot(number_of_CpGs_per_tile_2, aes(x = Number_of_CpGs, y = Number_of_tiles_having_corresponding_number_of_CpGs)) +
-  geom_bar(stat = "identity") +
-  labs(x = "Number_of_CpGs", y = "Number_of_tiles_having_corresponding_number_of_CpGs", title = "1kb tile and CpG overlap") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-head(granges(Chd1_1kb_tiled_filtered))
-
-# tilling Ctr2 sample
-
-Ctr2_1kb_tiled <- tile_by_windows(bs = Ctr2_BSseq, win_size = 1000)
-nrow(Ctr2_1kb_tiled) # 2,722,508 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Ctr2_1kb_tiled_filtered <- Ctr2_1kb_tiled[rowSums(as.matrix(countOverlaps(Ctr2_1kb_tiled, Ctr2_BSseq))) > 2,]
-nrow(Ctr2_1kb_tiled_filtered) # 2,242,752 tiles left! 479,756 tiles removed!
-
-# tilling Ctr6_1 sample
-
-Ctr6_1_1kb_tiled <- tile_by_windows(bs = Ctr6_1_BSseq, win_size = 1000)
-nrow(Ctr6_1_1kb_tiled) # 2,722,502 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Ctr6_1_1kb_tiled_filtered <- Ctr6_1_1kb_tiled[rowSums(as.matrix(countOverlaps(Ctr6_1_1kb_tiled, Ctr6_1_BSseq))) > 2,]
-nrow(Ctr6_1_1kb_tiled_filtered) # 2,242,969 tiles left!
-
-# tilling Ctr6_2 sample
-
-Ctr6_2_1kb_tiled <- tile_by_windows(bs = Ctr6_2_BSseq, win_size = 1000)
-nrow(Ctr6_2_1kb_tiled) # 2,722,504 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Ctr6_2_1kb_tiled_filtered <- Ctr6_2_1kb_tiled[rowSums(as.matrix(countOverlaps(Ctr6_2_1kb_tiled, Ctr6_2_BSseq))) > 2,]
-nrow(Ctr6_2_1kb_tiled_filtered) # 2,243,425 tiles left!
-
-# tilling Ctr6_3 sample
-
-Ctr6_3_1kb_tiled <- tile_by_windows(bs = Ctr6_3_BSseq, win_size = 1000)
-nrow(Ctr6_3_1kb_tiled) # 2,722,508 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Ctr6_3_1kb_tiled_filtered <- Ctr6_3_1kb_tiled[rowSums(as.matrix(countOverlaps(Ctr6_3_1kb_tiled, Ctr6_3_BSseq))) > 2,]
-nrow(Ctr6_3_1kb_tiled_filtered) # 2,311,551 tiles left!
-
-# tilling Kmt2a_1 sample
-
-Kmt2a_1_1kb_tiled <- tile_by_windows(bs = Kmt2a_1_BSseq, win_size = 1000)
-nrow(Kmt2a_1_1kb_tiled) # 2,722,504 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Kmt2a_1_1kb_tiled_filtered <- Kmt2a_1_1kb_tiled[rowSums(as.matrix(countOverlaps(Kmt2a_1_1kb_tiled, Kmt2a_1_BSseq))) > 2,]
-nrow(Kmt2a_1_1kb_tiled_filtered) # 2,243,282 tiles left!
-
-# tilling Kmt2a_2 sample
-
-Kmt2a_2_1kb_tiled <- tile_by_windows(bs = Kmt2a_2_BSseq, win_size = 1000)
-nrow(Kmt2a_2_1kb_tiled) # 2,722,506 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Kmt2a_2_1kb_tiled_filtered <- Kmt2a_2_1kb_tiled[rowSums(as.matrix(countOverlaps(Kmt2a_2_1kb_tiled, Kmt2a_2_BSseq))) > 2,]
-nrow(Kmt2a_2_1kb_tiled_filtered) # 2,722,506 tiles left!
-
-# tilling Kmt2a_3 sample
-
-Kmt2a_3_1kb_tiled <- tile_by_windows(bs = Kmt2a_3_BSseq, win_size = 1000)
-nrow(Kmt2a_3_1kb_tiled) # 2,722,508 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Kmt2a_3_1kb_tiled_filtered <- Kmt2a_3_1kb_tiled[rowSums(as.matrix(countOverlaps(Kmt2a_3_1kb_tiled, Kmt2a_3_BSseq))) > 2,]
-nrow(Kmt2a_3_1kb_tiled_filtered) # 2,312,208 tiles left!
-
-# tilling Crebbp sample
-
-Crebbp_1kb_tiled <- tile_by_windows(bs = Crebbp_BSseq, win_size = 1000)
-nrow(Crebbp_1kb_tiled) # 2,722,502 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Crebbp_1kb_tiled_filtered <- Crebbp_1kb_tiled[rowSums(as.matrix(countOverlaps(Crebbp_1kb_tiled, Crebbp_BSseq))) > 2,]
-nrow(Crebbp_1kb_tiled_filtered) # 2,243,613 tiles left!
-
-# tilling Ctr3 sample
-
-Ctr3_1kb_tiled <- tile_by_windows(bs = Ctr3_BSseq, win_size = 1000)
-nrow(Ctr3_1kb_tiled) # 2,722,502 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Ctr3_1kb_tiled_filtered <- Ctr3_1kb_tiled[rowSums(as.matrix(countOverlaps(Ctr3_1kb_tiled, Ctr3_BSseq))) > 2,]
-nrow(Ctr3_1kb_tiled_filtered) # 2,243,458 tiles left!
-
-# tilling Dnmt1 sample
-
-Dnmt1_1kb_tiled <- tile_by_windows(bs = Dnmt1_BSseq, win_size = 1000)
-nrow(Dnmt1_1kb_tiled) # 2,722,496 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Dnmt1_1kb_tiled_filtered <- Dnmt1_1kb_tiled[rowSums(as.matrix(countOverlaps(Dnmt1_1kb_tiled, Dnmt1_BSseq))) > 2,]
-nrow(Dnmt1_1kb_tiled_filtered) # 2,243,022 tiles left!
-
-# tilling Ezh2 sample
-
-Ezh2_1kb_tiled <- tile_by_windows(bs = Ezh2_BSseq, win_size = 1000)
-nrow(Ezh2_1kb_tiled) # 2,722,495 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Ezh2_1kb_tiled_filtered <- Ezh2_1kb_tiled[rowSums(as.matrix(countOverlaps(Ezh2_1kb_tiled, Ezh2_BSseq))) > 2,]
-nrow(Ezh2_1kb_tiled_filtered) # 2,243,195 tiles left!
-
-# tilling Hdac6 sample
-
-Hdac6_1kb_tiled <- tile_by_windows(bs = Hdac6_BSseq, win_size = 1000)
-nrow(Hdac6_1kb_tiled) # 2,722,502 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Hdac6_1kb_tiled_filtered <- Hdac6_1kb_tiled[rowSums(as.matrix(countOverlaps(Hdac6_1kb_tiled, Hdac6_BSseq))) > 2,]
-nrow(Hdac6_1kb_tiled_filtered) # 2,243,302 tiles left!
-
-# tilling Kdm6a sample
-
-Kdm6a_1kb_tiled <- tile_by_windows(bs = Kdm6a_BSseq, win_size = 1000)
-nrow(Kdm6a_1kb_tiled) # 2,722,493 tiles!
-
-# Count overlaps between tiled genome and CpG level genome and keep tiles with at least 3 CpGs
-
-Kdm6a_1kb_tiled_filtered <- Kdm6a_1kb_tiled[rowSums(as.matrix(countOverlaps(Kdm6a_1kb_tiled, Kdm6a_BSseq))) > 2,]
-nrow(Kdm6a_1kb_tiled_filtered) # 2,243,316 tiles left!
-
-# --- tile each BSseq object to 1kb length size, so each tile has certain number of CpGs end ---
-
-# --- Merging BSseq object start ---
-
-merged_BSseq <- combine(Chd1_1kb_tiled_filtered, Ctr2_1kb_tiled_filtered, Ctr6_1_1kb_tiled_filtered,
-                        Ctr6_2_1kb_tiled_filtered, Ctr6_3_1kb_tiled_filtered, Kmt2a_1_1kb_tiled_filtered,
-                        Kmt2a_2_1kb_tiled_filtered, Kmt2a_3_1kb_tiled_filtered, Crebbp_1kb_tiled_filtered,
-                        Ctr3_1kb_tiled_filtered, Dnmt1_1kb_tiled_filtered, Ezh2_1kb_tiled_filtered,
-                        Hdac6_1kb_tiled_filtered, Kdm6a_1kb_tiled_filtered)
-
-# Collapsedata
+# --- Merging BSseq objects start ---
+
+merged_BSseq <- combine(Chd1_BSseq, Ctr2_BSseq, Ctr6_1_BSseq,
+                        Ctr6_2_BSseq, Ctr6_3_BSseq, Kmt2a_1_BSseq,
+                        Kmt2a_2_BSseq, Kmt2a_3_BSseq, Crebbp_BSseq,
+                        Ctr3_BSseq, Ezh2_BSseq,
+                        Hdac6_BSseq, Kdm6a_BSseq)
 
 merged_BSseq <- collapseBSseq(merged_BSseq, group = c("Chd1", "Ctr2", "Ctr6_1", "Ctr6_2", "Ctr6_3", "Kmt2a_1", "Kmt2a_2",
-                                                      "Kmt2a_3", "Crebbp", "Ctr3", "Dnmt1", "Ezh2", "Hdac6", "Kdm6a"))
+                                                      "Kmt2a_3", "Crebbp", "Ctr3", "Ezh2", "Hdac6", "Kdm6a"))
 
 nrow(merged_BSseq) # 2,314,224 shared 1kb tiles!
 
-# Add sample name info to merged data
+# --- Merging BSseq objects end ---
+
+# --- Add sample name info to merged data start ---
 
 metadata <- data.frame("Samples" = c("Chd1", "Ctr2", "Ctr6_1", "Ctr6_2", "Ctr6_3", "Kmt2a_1", "Kmt2a_2",
-                                     "Kmt2a_3", "Crebbp", "Ctr3", "Dnmt1", "Ezh2", "Hdac6", "Kdm6a")) 
+                                     "Kmt2a_3", "Crebbp", "Ctr3", "Ezh2", "Hdac6", "Kdm6a")) 
 # sample description with metadata
 
 pData(merged_BSseq)$metadata <- metadata
 pData(merged_BSseq)
 
-# --- Merging BSseq object end ---
+# --- Add sample name info to merged data end ---
 
-# --- Coverage filtering for 1kb tile data start ---
+# --- Filtering number of CpGs with 0 coverage in all samples start ---
+
+merged_BSseq.cov <- getCoverage(merged_BSseq) # 21,860,443 CpGs in total
+keep <- which(rowSums(merged_BSseq.cov) !=0) # 21,858,950 CpG has > 0 coverage in all samples
+merged_BSseq_filtered <- merged_BSseq[keep,]
+nrow(merged_BSseq_filtered) # 21,858,950 CpGs left!
+
+# --- Filtering number of CpGs with 0 coverage in all samples end ---
+
+# --- Tilling CpGs start ---
+
+merged_BSseq_tiled <- tile_by_windows(bs = merged_BSseq_filtered, win_size = 1000) # 2,722,508 1kb length tiles!
+
+# --- Tilling CpGs end ---
+
+# --- Keeping tiles with at least 3 CpGs start ---
+
+merged_BSseq_tiled_filtered <- merged_BSseq_tiled[rowSums(as.matrix(countOverlaps(merged_BSseq_tiled, merged_BSseq_filtered))) > 2,]
+
+# 2,314,109 1kb lenght tiles left! 408,399 tiles are removed!
+
+# --- Keeping tiles with at least 3 CpGs end ---
+
+# --- Checking coverage of 1kb tile data start ---
 
 ## Number of tiles with 0 coverage in all samples
 
-sum(rowSums(getCoverage(merged_BSseq)) == 0) # 50 tiles have zero coverage in all samples!
-which(rowSums(getCoverage(merged_BSseq)) == 0) # checking which rows have zero coverage in all samples.
-getCoverage(merged_BSseq)[1753879,] # 1753879th row has zero coverage in all samples.
-getCoverage(merged_BSseq)[2156745,] # 2156745th row has zero coverage in all samples.
+sum(rowSums(getCoverage(merged_BSseq_tiled_filtered)) == 0) # zero tile with zero coverage in all samples!
+which(rowSums(getCoverage(merged_BSseq_tiled_filtered)) == 0)
 
-# Filtering tiles with 0 coverage in all samples
-
-merged_BSseq.cov <- getCoverage(merged_BSseq)
-keep <- which(rowSums(merged_BSseq.cov) !=0)
-merged_BSseq_2 <- merged_BSseq[keep,]
-nrow(merged_BSseq_2)
-
-# --- Coverage filtering for 1kb tile data end ---
+# --- Checking coverage of 1kb tile data end ---
 
 # --- Visualization of tiled CpG sites start ---
 
-head(granges(merged_BSseq_2)) # see 1kb tiled coordinates
-head(getCoverage(merged_BSseq_2)) # get coverage of data
-round(colMeans(getCoverage(merged_BSseq_2)), 1) # average coverage of CpG tiles!
+head(granges(merged_BSseq_tiled_filtered)) # see 1kb tiled coordinates
+head(getCoverage(merged_BSseq_tiled_filtered)) # get coverage of data
+round(colMeans(getCoverage(merged_BSseq_tiled_filtered)), 1) # average coverage of CpG tiles!
 
 # --- Visualization of tiled CpG sites end ---
 
@@ -416,12 +245,12 @@ round(colMeans(getCoverage(merged_BSseq_2)), 1) # average coverage of CpG tiles!
 
 # Here raw data used instead of smoothed data
 
-merged_BSseq_methylation <- getMeth(merged_BSseq_2, type = "raw")
-nrow(merged_BSseq_methylation) # 2,314,174 tiles
+merged_BSseq_methylation <- getMeth(merged_BSseq_tiled_filtered, type = "raw")
+nrow(merged_BSseq_methylation) # 2,314,109 tiles
 
 # --- get methylation matrix from filtered data with getMeth() function end ---
 
-# --- Filtering non-overlapping tiles between samples which returns NA values in rows start ---
+# --- Filtering tiles with NA methylation values in at least one of the samples start ---
 
 sum(rowSums(is.na(merged_BSseq_methylation)) > 0) # how many rows (tiles) have NA values
 sum(apply(merged_BSseq_methylation, 1, anyNA)) # how many rows (tiles) have NA values
@@ -429,18 +258,24 @@ table(apply(merged_BSseq_methylation, 1, function(X) any(is.na(X)))) # how many 
 which_NAs <- apply(merged_BSseq_methylation, 1, function(X) any(is.na(X))) # how many rows (tiles) have NA values
 length(which(which_NAs)) # how many rows (tiles) have NA values
 which(which_NAs) # Which rows (tiles) have NA values?
-merged_BSseq_methylation[2116940,] # 2116940th tile has NA value
-merged_BSseq_methylation[2139948,] # 2139948th tile has NA value
+merged_BSseq_methylation[1238370,] # 1238370th tile has NA value
+merged_BSseq_methylation[1238557,] # 1238557th tile has NA value
 
-# There are 74,667 NA values, that is, there are 74,667 non-overlapping CpG tiles
+# There are 74,449 NA values, that is, there are 74,449 1kb tiles have NA methylation values in at least one of the samples
 
 # Removing CpG tiles with NA values
 
 merged_BSseq_methylation_filtered <- merged_BSseq_methylation[rowSums(is.na(merged_BSseq_methylation)) == 0,] # Removal NA values
 sum(rowSums(is.na(merged_BSseq_methylation_filtered)) > 0)  # How many tiles have NA values?
-nrow(merged_BSseq_methylation_filtered) # 2,239,507 overlapping tiles left across samples!
+nrow(merged_BSseq_methylation_filtered) # 2,239,660 tiles left across samples!
 
-# --- Filtering non-overlapping tiles between samples which returns NA values in rows end ---
+# --- Filtering tiles with NA methylation values in at least one of the samples end ---
+
+# --- Sum of methylation values in each sample start ---
+
+colSums(merged_BSseq_methylation_filtered)
+
+# --- Sum of methylation values in each sample end ---
 
 # --- PCA plot start ---
 
@@ -448,12 +283,10 @@ pca_data=prcomp(t(merged_BSseq_methylation_filtered))
 pca_data_perc=round(100*pca_data$sdev^2/sum(pca_data$sdev^2),1)
 df_pca_data = data.frame(PC1 = pca_data$x[,1], PC2 = pca_data$x[,2], sample = colnames(merged_BSseq_methylation_filtered),
                          Samples = c("Chd1", "Ctr2", "Ctr6_1", "Ctr6_2", "Ctr6_3", "Kmt2a_1", "Kmt2a_2",
-                                       "Kmt2a_3", "Crebbp", "Ctr3", "Dnmt1", "Ezh2", "Hdac6", "Kdm6a"))
+                                     "Kmt2a_3", "Crebbp", "Ctr3", "Ezh2", "Hdac6", "Kdm6a"))
 
 ggplot(df_pca_data, aes(PC1,PC2, color = Samples,label=row.names(df_pca_data))) +
   geom_point(size=8)+ labs(x=paste0("PC1 (",pca_data_perc[1],")"), y=paste0("PC2 (",pca_data_perc[2],")")) +
   geom_text(nudge_x = 2.5,nudge_y = 2.5, size = 5) + theme_classic()
 
 # --- PCA plot end ---
-
-# ------------------------------------------- End -------------------------------------------
