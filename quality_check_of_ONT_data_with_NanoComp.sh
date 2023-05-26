f12ff4437b20813af8a9ab7f25577767c5b981df
Kaan Okay <42235353+kaanokay@users.noreply.github.com>
2023-05-26 20:58:30 +0000

    Add files via upload

---
+++ quality_check_of_ONT_data_with_NanoComp.sh
@@ -0,0 +1,22 @@
+# Quality control of ONT data, including read length, number of total reads, number of sequenced base etc...
+
+NanoComp --summary \
+Chd1_sequencing_summary_PAG72824_78389ed5_958278a9.txt \
+Crebbp_sequencing_summary_PAG79846_6f2d04ce_4ae0b113.txt \
+Ctr1_sequencing_summary_PAG84699_146aca3d_233a46f0.txt \
+Ctr2_sequencing_summary_PAG70393_6269df8d_bd40fa83.txt \
+Ctr3_sequencing_summary_PAG79846_6f2d04ce_4ae0b113.txt \
+Ctr6_1_sequencing_summary_PAM35058_798a467a_2a97ec6d.txt \
+Ctr6_2_sequencing_summary_PAM35039_4a0c1075_8a40e3e1.txt \
+Dnmt1_sequencing_summary_PAG79945_ec4ad5ab_a8fae465.txt \
+Ezh2_sequencing_summary_PAM39350_cbfbdba3_20e1311c.txt \
+Hdac6_sequencing_summary_PAG80090_eac26dc6_3cebbe70.txt \
+Hdac8_sequencing_summary_PAG80022_5a69b237_6dd2d720.txt \
+Kdm1a_sequencing_summary_PAM39304_4efbd3b3_d0bc228c.txt \
+Kdm2b_sequencing_summary_PAG84405_3ff81c5f_98a175d9.txt \
+Kdm5b_sequencing_summary_PAM37619_d1e3b470_3593271c.txt \
+Kdm5c_sequencing_summary_PAM35061_4d505f8a_6f23b70b.txt \
+Kdm6a_sequencing_summary_PAG80109_eb89aa54_9f6b9123.txt \
+Kmt2a_1_sequencing_summary_PAG72927_d35c59b3_535cc4e1.txt \
+Kmt2a_2_sequencing_summary_PAG84277_b5d6935b_d3fb9732.txt \
+--names Chd1 Crebbp Ctr1 Ctr2 Ctr3 Ctr6_1 Ctr6_2 Dnmt1 Ezh2 Hdac6 Hdac8 Kdm1a Kdm2b Kdm5b Kdm5c Kdm6a Kmt2a_1 Kmt2a_2 --outdir compare-runs --threads 128
